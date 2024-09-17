948010081cca472e89def5283dc7ce1b92423b31
brightini <brightiniabasi@gmail.com>
2024-09-17 05:28:01 +0100

    Restructured and added files for backend

---
+++ part2/phonebook/backend/index.js
@@ -0,0 +1,83 @@
+import express, { response } from "express";
+const app = express();
+import cors from "cors";
+import Person from "./models/person.js";
+
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
+});
+
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) response.json(person);
+      else response.status(404).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const nameToCheck = request.body.name;
+  const newPhoneNumber = request.body.number;
+
+  Person.findOneAndUpdate(
+    { name: nameToCheck },
+    { $set: { phoneNumber: newPhoneNumber } },
+    { new: true }
+  )
+    .then((updatedContact) => {
+      response.json(updatedContact);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body;
+
+  const person = new Person({
+    name: body.name,
+    phoneNumber: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({
+      error: "Invalid ID format",
+    });
+  } else if (error.name === "ValidationError") {
+    console.log(error.message);
+
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
+
+const PORT = 3002;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ part2/phonebook/backend/models/person.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const url = process.env.MONGODB_URI;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    validate: {
+      validator: function (value) {
+        return /^\d{2,3}-\d{5,}$/.test(value);
+      },
+      message: (props) => `${props.value} is not a valid number`,
+    },
+    required: true,
+  },
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export default mongoose.model("Person", personSchema);
