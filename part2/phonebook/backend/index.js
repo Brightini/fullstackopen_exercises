e9d6ccbc87a5a32f672e663e74f84f180c31ed05
brightini <brightiniabasi@gmail.com>
2024-09-26 11:22:39 +0100

    Lint fixes

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -1,30 +1,30 @@
-import express, { response } from "express";
-const app = express();
-import cors from "cors";
-import Person from "./models/person.js";
+import express, { response } from 'express'
+const app = express()
+import cors from 'cors'
+import Person from './models/person.js'
 
-app.use(express.static("dist"));
-app.use(express.json());
-app.use(cors());
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
-      if (person) response.json(person);
-      else response.status(404).end();
+      if (person) response.json(person)
+      else response.status(404).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const nameToCheck = request.body.name;
-  const newPhoneNumber = request.body.number;
+app.put('/api/persons/:id', (request, response, next) => {
+  const nameToCheck = request.body.name
+  const newPhoneNumber = request.body.number
 
   Person.findOneAndUpdate(
     { name: nameToCheck },
@@ -32,52 +32,52 @@ app.put("/api/persons/:id", (request, response, next) => {
     { new: true }
   )
     .then((updatedContact) => {
-      response.json(updatedContact);
+      response.json(updatedContact)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   const person = new Person({
     name: body.name,
     phoneNumber: body.number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-  if (error.name === "CastError") {
+  console.error(error.message)
+  if (error.name === 'CastError') {
     return response.status(400).send({
-      error: "Invalid ID format",
-    });
-  } else if (error.name === "ValidationError") {
-    console.log(error.message);
+      error: 'Invalid ID format',
+    })
+  } else if (error.name === 'ValidationError') {
+    console.log(error.message)
 
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
-app.use(errorHandler);
+  next(error)
+}
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part2/phonebook/backend/models/person.js
+++ part2/phonebook/backend/models/person.js
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-dotenv.config();
-const url = process.env.MONGODB_URI;
+dotenv.config()
+const url = process.env.MONGODB_URI
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB')
   })
   .catch((error) => {
-    console.log("Error connecting to MongoDB", error.message);
-  });
+    console.log('Error connecting to MongoDB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -24,20 +24,20 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function (value) {
-        return /^\d{2,3}-\d{5,}$/.test(value);
+        return /^\d{2,3}-\d{5,}$/.test(value)
       },
       message: (props) => `${props.value} is not a valid number`,
     },
     required: true,
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-export default mongoose.model("Person", personSchema);
+export default mongoose.model('Person', personSchema)
